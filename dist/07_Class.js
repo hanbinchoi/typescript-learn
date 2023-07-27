2270a9d9fc4c6e6a10da2d2097bfc8216194acf0
hanbinchoi <wkghskak@naver.com>
2023-07-27 15:02:31 +0900

    Class2

--- dist/07_Class.js
+++ dist/07_Class.js
@@ -16,4 +16,32 @@ class Counter {
 let counter1 = new Counter();
 counter1.increase();
 console.log(counter1.getValue());
+class Animal {
+  constructor(theName) {
+    this.name = theName;
+  }
+  speak(sound = "왕왕!") {
+    console.log(`${this.name}(은/는) ${sound}하고 웁니다.`);
+  }
+}
+class Mouse extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  speak(sound = "찍찍") {
+    super.speak(sound);
+  }
+}
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  speak(sound = "야옹") {
+    super.speak(sound);
+  }
+}
+let jerry = new Mouse("제리");
+let tom = new Cat("톰");
+jerry.speak();
+tom.speak("냥냥");
 //# sourceMappingURL=07_Class.js.map
